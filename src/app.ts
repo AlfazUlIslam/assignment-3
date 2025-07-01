8ac862cfb1e768a920042f4476972255a72c08b9
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2025-07-01 23:55:18 +0600

    CORS added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
 import bookRouter from "./routes/book.routes";
 import borrowRouter from "./routes/borrow.routes";
 
 const app = express();
+const allowedOrigins = ['http://localhost:5173'];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/books", bookRouter);
